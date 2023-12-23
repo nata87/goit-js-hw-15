4d8b3710ad2caaba342f76e0aa0fcb829bf9bf4a
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2023-12-23 20:15:45 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements["email"];
+  const passwordInput = loginForm.elements["password"];
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("всі поля повинні бути заповнені.!");
+    return;
+  }
+
+  const formDataCreation = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  loginForm.reset();
+});
